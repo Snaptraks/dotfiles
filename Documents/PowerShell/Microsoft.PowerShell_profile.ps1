4ba608826110148dbe4998fe8563c1c711cd416f
Snaptraks <snaptraks@gmail.com>
2024-12-16 14:04:37 -0500

    Add Documents/PowerShell/Microsoft.PowerShell_profile.ps1

---
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,9 @@
+oh-my-posh init pwsh --config "$env:USERPROFILE\snaptraks.omp.json" | Invoke-Expression
+
+. "$env:HOME\Documents\PowerShell\Scripts\PyAutoEnv.ps1"
+
+#f45873b3-b655-43a6-b217-97c00aa0db58 PowerToys CommandNotFound module
+
+Import-Module -Name Microsoft.WinGet.CommandNotFound
+#f45873b3-b655-43a6-b217-97c00aa0db58
+
