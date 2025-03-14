3a9834a55cda9b9844c1def2f839bb8ff0f80417
Snaptraks <snaptraks@gmail.com>
2025-03-14 11:50:44 -0400

    Update Microsoft.PowerShell_profile.ps1

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,40 @@
-oh-my-posh init pwsh --config "$env:USERPROFILE\snaptraks.omp.json" | Invoke-Expression
+Set-PSReadLineOption –HistoryNoDuplicates:$True
+
+Set-Alias grep findstr
+Set-Alias ll ls
+Set-Alias npp 'C:\Program Files\Notepad++\notepad++.exe'
+
+function md5 { Get-FileHash $args -Algorithm MD5 }
+function sha256 { Get-FileHash $args -Algorithm SHA256 }
+function lastwake { powercfg -lastwake }
+function ytdl { uvx --from yt-dlp[default] yt-dlp $args }
+
+Function Get-DU {
+    Param (
+        $Path = "."
+    )
+    $AllFileInfo = @()
+    ForEach ($File in (Get-ChildItem $Path)) {
+        If ($File.PSisContainer) {
+            $Size = [Math]::Round((Get-ChildItem $File.FullName -Recurse | Measure-Object -Property Length -Sum).Sum / 1KB, 2)
+            $Folder = "True"
+        }
+        Else {
+            $Size = $File.Length
+            $Folder = ""
+        }
+        $FileInfo = New-Object System.Object
+        $FileInfo | Add-Member -type NoteProperty -name Name -value $File.name
+        $FileInfo | Add-Member -type NoteProperty -name Folder -value $Folder
+        $FileInfo | Add-Member -type NoteProperty -name Size -value $Size
+        $AllFileInfo += $FileInfo
+    }
+    $AllFileInfo
+}
+Set-Alias du Get-DU
+
+oh-my-posh init pwsh --config "$env:USERPROFILE/snaptraks.omp.json" | Invoke-Expression
+
 
 . "$env:HOME\Documents\PowerShell\Scripts\PyAutoEnv.ps1"
 
@@ -6,4 +42,3 @@ oh-my-posh init pwsh --config "$env:USERPROFILE\snaptraks.omp.json" | Invoke-Exp
 
 Import-Module -Name Microsoft.WinGet.CommandNotFound
 #f45873b3-b655-43a6-b217-97c00aa0db58
-
