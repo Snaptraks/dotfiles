c90495ba262a62d4716c50bfd372b55ecf153f46
Snaptraks <snaptraks@gmail.com>
2025-03-15 12:36:16 -0400

    Fix Powershell module

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -36,7 +36,7 @@ Set-Alias du Get-DU
 oh-my-posh init pwsh --config "$env:USERPROFILE/snaptraks.omp.json" | Invoke-Expression
 
 
-. "$env:HOME\Documents\PowerShell\Scripts\PyAutoEnv.ps1"
+. "$env:USERPROFILE\Documents\PowerShell\Scripts\PyAutoEnv.ps1"
 
 #f45873b3-b655-43a6-b217-97c00aa0db58 PowerToys CommandNotFound module
 
