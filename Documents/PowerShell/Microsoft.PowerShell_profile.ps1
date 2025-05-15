63111481ef89fc8b3e0cdf6895e406224607c6de
Snaptraks <snaptraks@gmail.com>
2025-05-15 11:23:15 -0400

    Update prompt configuration

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -38,6 +38,8 @@ oh-my-posh init pwsh --config "$env:USERPROFILE/snaptraks.omp.json" | Invoke-Exp
 
 . "$env:USERPROFILE\Documents\PowerShell\Scripts\PyAutoEnv.ps1"
 
+$env:VIRTUAL_ENV_DISABLE_PROMPT = 1
+
 #f45873b3-b655-43a6-b217-97c00aa0db58 PowerToys CommandNotFound module
 
 Import-Module -Name Microsoft.WinGet.CommandNotFound
